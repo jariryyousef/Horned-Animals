6775b0f416266cda6e8d4fccd4a2a802ab3e460c
jariryyousef <jariryyousef@gmail.com>
2021-08-29 22:17:37 +0300

    finsh the lab

--- horned-beasts/src/App.js
+++ horned-beasts/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import Header from './components/header';
+import Main from './components/Main';
+import Footer from './components/footer';
+
+// import HorendBeasts from './components/HorendBeasts';
+class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+    <Header/>
+    <Main/>
+    <Footer/>
     </div>
   );
-}
+}}
 
 export default App;
---
+++ horned-beasts/src/components/footer.js
@@ -0,0 +1,14 @@
+import React from"react";
+
+class Footer extends React.Component{
+
+render(){
+    return (
+        <p>Yousef Jariry</p>
+
+    )
+}
+
+}
+
+export default Footer;
