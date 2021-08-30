fd5de45b63446aeea65decc6e02645af8ea15724
jariryyousef <jariryyousef@gmail.com>
2021-08-31 00:55:49 +0300

    finsh the lab

--- horned-beasts/src/components/HorendBeasts.js
+++ horned-beasts/src/components/HorendBeasts.js
@@ -1,15 +1,43 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
+import Button from "react-bootstrap/Button";
 
 class HorendBeasts extends React.Component{
 
+
+    constructor(props){
+        super(props);
+        this.state={
+        numOfVafr: 0,
+
+        }
+    }
+    numOfVafr = () =>{
+        this.setState({
+
+            numOfVafr: this.state.numOfVafr + 1
+        })
+        
+    }
+
+
 render(){
     return(
-
-        <div>
-        <h2>{this.props.title}</h2>
-        <p>{this.props.description}</p>
-        <img src={this.props.img} alt=""/>
-        </div>
+<div>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img  variant="top" src={this.props.image_url} onClick={this.numOfVafr} />
+  <Card.Body>
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+    {this.props.description}
+    </Card.Text>
+    <Button onClick={this.numOfVafr} variant="primary">Like</Button>
+    <p>❤️ {this.state.numOfVafr}</p>
+  </Card.Body>
+</Card>
+</div>
+        
     )
 
 
